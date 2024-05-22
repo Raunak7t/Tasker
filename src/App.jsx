082969021b7e4de246873ffc3f409c4616d79a21
Raunak Tiwari <113747174+Raunak7t@users.noreply.github.com>
2024-05-22 19:14:28 +0530

    Start

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>Tasker</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    screens: {
+      mobile: { max: "767px" },
+    },
+  },
+  plugins: [],
+};
